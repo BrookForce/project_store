44735549db177f459c66185697e06e28d2fd9118
Lucas Ribeiro <lucasmarquesribeiro2003@gmail.com>
2024-05-23 11:24:31 +0100

    chore: Update Firebase import statements to use the new modular syntax

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
-export { onAuthStateChanged };
+export { auth, onAuthStateChanged };
 
     
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-        <h1>Criar conta</h1>
+        <h1>Create Account</h1>
         <p><input type='text' placeholder='Email' v-model='email'/></p>
         <p><input type='password' placeholder='Password' v-model='password'/></p>
-        <p><button @click='register'>Criar conta</button></p>
-        <p><button @click='signInWithGoogle'>Criar conta com o Google</button></p>
+        <p><button @click='register'>Create Account</button></p>
+        <p><button @click='signInWithGoogle'>Create Account with Google</button></p>
     </div>
 </template>
 
 <script>
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/compat/auth';
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 export default {
   data() {
@@ -20,8 +20,10 @@ export default {
   },
   methods: {
     register() {
-      createUserWithEmailAndPassword(getAuth(), this.email, this.password)
-        .then((data) => {
+      const auth = getAuth(); // Obtenha a instância de autenticação
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+            const user = userCredential.user;
             console.log('success');
             this.$router.push('/about');
         })
@@ -32,7 +34,8 @@ export default {
     },
     signInWithGoogle() {
       const provider = new GoogleAuthProvider();
-      signInWithPopup(getAuth(), provider)
+      const auth = getAuth(); // Obtenha a instância de autenticação
+      signInWithPopup(auth, provider)
         .then((result) => {
             console.log(result.user);
             this.$router.push('/about');
